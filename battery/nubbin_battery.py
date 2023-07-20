17b37d95fa7bbd9dd406216776acaa57b5c7d506
Harish <harishbb168@gmail.com>
2023-07-20 09:19:48 +0530

    Unit testing added, also some corrections done in code to resolve failing tests.

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,5 +1,5 @@
-from ..utils import add_years_to_date
-from .battery import Battery
+from utils import add_years_to_date
+from battery.battery import Battery
 
 
 class NubbinBattery(Battery):
@@ -10,7 +10,7 @@ class NubbinBattery(Battery):
 
     def needs_service(self):
         date_which_battery_should_be_serviced_by = add_years_to_date(
-            self.last_service_date, 2)
+            self.last_service_date, 4)
         if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
